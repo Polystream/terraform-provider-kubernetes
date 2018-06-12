c5b98460eeda747f3fb4935d39c1da7126eb9e35
Mike Norgate <mike@polystream.com>
2018-06-12 11:56:56 +0100

    Stop update of environment variables creating new resource

--- kubernetes/schema_container.go
+++ kubernetes/schema_container.go
@@ -216,8 +216,8 @@ func containerFields(isUpdatable, isInitContainer bool) map[string]*schema.Schem
 		"env": {
 			Type:        schema.TypeList,
 			Optional:    true,
-			ForceNew:    true,
-			Description: "List of environment variables to set in the container. Cannot be updated.",
+			ForceNew:    false,
+			Description: "List of environment variables to set in the container.",
 			Elem: &schema.Resource{
 				Schema: map[string]*schema.Schema{
 					"name": {
@@ -227,7 +227,7 @@ func containerFields(isUpdatable, isInitContainer bool) map[string]*schema.Schem
 					},
 					"value": {
 						Type:        schema.TypeString,
-						ForceNew:    true,
+						ForceNew:    false,
 						Optional:    true,
 						Description: `Variable references $(VAR_NAME) are expanded using the previous defined environment variables in the container and any service environment variables. If a variable cannot be resolved, the reference in the input string will be unchanged. The $(VAR_NAME) syntax can be escaped with a double $$, ie: $$(VAR_NAME). Escaped references will never be expanded, regardless of whether the variable exists or not. Defaults to "".`,
 					},
